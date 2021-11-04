96864245837ac687d158555a91c5bce4ea12277d
SathishKumar Rajendran <90351356+sathishkumar130913@users.noreply.github.com>
2021-11-04 11:51:09 +0000

    Add project files.

---
+++ clsRules.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Jenkins.CoreApp;
+namespace Jen.WebApp
+{
+       public class clsRules
+    {
+        RuleLogic objRulelogic = new RuleLogic();
+        public int Addlogic(int a, int b)
+        {
+            return objRulelogic.Sumlogic(5, 5);
+        }
+    }
+}
